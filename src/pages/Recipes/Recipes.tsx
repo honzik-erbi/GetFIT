21fedaae912350cd668c93d68e1f9bf5f0c0265d
honzik-erbi <jan.erben@student.spsmb.cz>
2024-03-22 09:28:45 +0100

    changed the title of the app window, recipes page work and implementing retrieving id from json logic(not working yet)

--- src/pages/Recipes/Recipes.tsx
+++ src/pages/Recipes/Recipes.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import Navbar from "../../components/Navbar/Navbar";
 import Card from "../../components/Card/Card";
-import RecipeData from "../../data/Recipes/recipe.json"
+import BreakfastRecipeData from "../../data/Recipes/breakfast.json"
 import { Box, Columns,} from "react-bulma-components";
 import Recipe from "../../components/Recipe/Recipe";
-import RecipeGuideData from "../../data/RecipeGuide/recipeguide.json"
+import BreakfastGuideData from "../../data/RecipeGuide/breakfast.json"
 import RecipeGuide from "../../components/RecipeGuide/RecipeGuide";
 
 //zde budou videa či fotky pro pochopení cviků a recepty popsané podrobně
@@ -19,16 +19,17 @@ export default function Recipes(props: any) {
       </Box>
       <>
         <Columns>
-        {RecipeData.map((element, index) => (
+        {BreakfastRecipeData.map((element, _id) => (
           <Columns.Column>
-          <Recipe key={index} {...element} />
+          <Recipe key={1} {...element} />
           </Columns.Column>
         ))}
-        {RecipeGuideData.map((element, index) => (
+        {BreakfastGuideData.map((element, index) => (
           <RecipeGuide key={index} {...element} />
         ))}
         </Columns>
       </>
     </>
+
   );
 }
