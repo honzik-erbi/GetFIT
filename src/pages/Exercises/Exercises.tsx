5db36d7016e4054627e295ba1041c508f8ff09b9
honzik-erbi <jan.erben@student.spsmb.cz>
2024-04-03 13:43:15 +0100

    added dropdown select menu for exercises

--- src/pages/Exercises/Exercises.tsx
+++ src/pages/Exercises/Exercises.tsx
@@ -1,14 +1,29 @@
-import { Link } from "react-router-dom";
 import React from "react";
 import Navbar from "../../components/Navbar/Navbar";
+import { Box } from "react-bulma-components";
 
 //zde budou cvičební plány rozdělené do 3 skupin: Beginner/Intermediate/Skilled
 
 export default function Exercises(props: any) {
   return (
     <>
-    <Navbar selected={props.selected}/>
-      <h1>Exercises</h1>
+      <>
+        <Navbar selected={props.selected} />
+        <Box>
+          <h1>Exercises</h1>
+        </Box>
+      </>
+      <div className="select">
+        <select>
+          <option>Select exercises</option>
+          <option>Chest</option>
+          <option>Back</option>
+          <option>Legs</option>
+          <option>Biceps</option>
+          <option>Triceps</option>
+          <option>Shoulders</option>
+        </select>
+      </div>
     </>
   );
 }
